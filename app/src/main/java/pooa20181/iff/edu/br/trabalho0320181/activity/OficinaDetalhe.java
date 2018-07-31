9cb6add625c3dbb03f14c549d640c8b3dff22d4d
Lucas <lucasmisidoro@gmail.com>
2018-07-31 14:05:28 -0300

    adicionando atributos

--- app/src/main/java/pooa20181/iff/edu/br/trabalho0320181/activity/OficinaDetalhe.java
+++ app/src/main/java/pooa20181/iff/edu/br/trabalho0320181/activity/OficinaDetalhe.java
@@ -2,9 +2,15 @@ package pooa20181.iff.edu.br.trabalho0320181.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import pooa20181.iff.edu.br.trabalho0320181.R;
 
 public class OficinaDetalhe extends AppCompatActivity {
+
+    EditText edtNome, edtRua, edtMunicipio, edtLongitude, edtLatitude;
+    Button btnBuscar, btnAdicionar, btnAlterar, btnExcluir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
