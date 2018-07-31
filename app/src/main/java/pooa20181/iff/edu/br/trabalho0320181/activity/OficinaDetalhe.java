e01ca452857fa976e78ac60cc4115af34ce3dc87
Lucas <lucasmisidoro@gmail.com>
2018-07-31 12:09:32 -0300

    Criação da Activity Oficina detalhe

---
+++ app/src/main/java/pooa20181/iff/edu/br/trabalho0320181/activity/OficinaDetalhe.java
@@ -0,0 +1,14 @@
+package pooa20181.iff.edu.br.trabalho0320181.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import pooa20181.iff.edu.br.trabalho0320181.R;
+
+public class OficinaDetalhe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oficina_detalhe);
+    }
+}
