9e3407d06e45692e2bd2aa1b139f4580ba9dac03
Lucas <lucasmisidoro@gmail.com>
2018-08-01 14:38:17 -0300

    implementação do layout mecanico detalhe

---
+++ app/src/main/java/pooa20181/iff/edu/br/trabalho0320181/activity/MecanicoDetalhe.java
@@ -0,0 +1,15 @@
+package pooa20181.iff.edu.br.trabalho0320181.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pooa20181.iff.edu.br.trabalho0320181.R;
+
+public class MecanicoDetalhe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mecanico_detalhe);
+    }
+}
